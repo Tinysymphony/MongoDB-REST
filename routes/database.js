b2db518a9d4ff742c44e8cf33e85e1ff7098b7f2
Tiny <zjutiny@gmail.com>
2015-07-26 15:57:36 +0800

    bulk insert by uploading binary files ( without validation )

--- routes/database.js
+++ routes/database.js
@@ -1,4 +1,4 @@
-var url = 'mongodb://127.0.0.1:27017/node6';
+var url = 'mongodb://127.0.0.1:27017/node8';
 var collection = 'taxi';
 var mongo = require('../lib/mongoCRUD').init(url);
 var mongoose = require('../lib/mongooseCRUD').init(url, collection);
@@ -11,7 +11,7 @@ var formidable = require('formidable');
 
 var database = express.Router();
 
-var tmpDirection = path.resolve(__dirname, '../dataFiles');
+var saveDirectory = path.resolve(__dirname, '../dataFiles');
 
 database.get('/taxi', function (req, res) {
     if(!req.body.query) { 
@@ -61,17 +61,45 @@ database.post('/taxi', function (req, res) {
 database.post('/taxi/file',  function (req, res, next){
     var form = new formidable.IncomingForm();
     form.encoding = 'utf-8';
-    form.uploadDir = tmpDirection;
+    form.uploadDir = saveDirectory;
     form.keepExtensions = false;
     form.type = 'multipart';
+    form.multiples = true;
 
-    form.parse(req, function (err ,fields, files){
-        if(err) { res.send(err.toString() + '\n'); return; }
-        console.log(files.fulAvatar.type);
-        fs.rename(files.fulAvatar.path, "dataFiles/tiny-" + new Date(), function(err){
-            if(err) { res.send(err.toString() + '\n'); return; }
-            res.send("ok!");
-        });
+    async.waterfall([
+        //save the upload files to tmp directory.
+        function(callback){
+            var filelist = [];
+            form.parse(req, function (err ,fields, files){
+                if(err) { callback(err); return; }
+                console.log(fields);
+                async.each(files.recordFiles, function(file, cb){
+                    var fileSavePath = path.join(saveDirectory, file.name);
+                    fs.rename(file.path, fileSavePath, function(err){
+                        if(err) { cb(err); return; }
+                        filelist.push(fileSavePath);
+                        cb();
+                    });
+                },  function(err){
+                    if(err) { callback(err); return; }
+                    callback(null, filelist);
+                });
+            });
+        },
+        //call save data to mongodb
+        function(filelist, callback){
+            mongoose.insertFiles(filelist, function (err, recordCounter){
+                if(err) callback(err);
+                callback(null, recordCounter);
+            });
+        }
+    ], function(err, recordCounter){
+        if(err) { res.send(err.toString() + "\n"); return ;}
+        if(err instanceof Array) {
+            res.send("Cannot save the following file(s):\n" + err.toString() + "\n"); 
+            return;
+        }
+        res.send("Success : " + recordCounter + " records have been inserted.\n");
     });
 
 });
